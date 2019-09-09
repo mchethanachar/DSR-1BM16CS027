5bc99ba5ebf8d871a46358353dcffe3bc6c698d5
Chethan M Achar <mchethan.achar@gmail.com>
2019-09-09 21:46:15 +0530

    Create Program-2.R

---
+++ Program-2.R
@@ -0,0 +1,9 @@
+Buff_Tail<-c(10,1,37,5,12)
+Garden_bee<-c(8,3,9,6,4)
+Red_Tail<-c(18,9,12,4,7)
+Carder_bee<-c(8,27,6,32,23)
+Honey_Bee<-c(12,13,16,9,10)
+
+dt=data.frame("Thistle"=Buff_Tail,"Vipers"=Garden_bee,"Golden Rain"=Red_Tail,"Yellowalfala"=Carder_bee, "blackberry"=Honey_Bee)
+dt
+
